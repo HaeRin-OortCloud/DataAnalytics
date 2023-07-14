28706ebeb96197d045b8134656094f418e87b61a
Haerin <109455136+HaeRin-OortCloud@users.noreply.github.com>
2023-07-14 16:20:31 +0900

    Update User3_Comparing New vs. Repeat Sessions by Channel.sql

--- User3_Comparing New vs. Repeat Sessions by Channel.sql
+++ User3_Comparing New vs. Repeat Sessions by Channel.sql
@@ -3,7 +3,7 @@
 USE mavenfuzzyfactory
 
 SELECT
-	utm_source,
+    utm_source,
     utm_campaign,
     http_referer,
     COUNT(CASE WHEN is_repeat_session = 0 THEN website_session_id ELSE NULL END) AS new_sessions,
@@ -18,7 +18,7 @@ ORDER BY 5 DESC
 
 SELECT
 	CASE
-		WHEN utm_source IS NULL AND http_referer IS NULL THEN 'direct_type_in'
+	WHEN utm_source IS NULL AND http_referer IS NULL THEN 'direct_type_in'
         WHEN utm_source IS NULL AND http_referer IN ('https://www.gsearch.com', 'https://www.bsearch.com') THEN 'organic_search'
         WHEN utm_campaign = 'nonbrand' THEN 'paid_nonbrand'
         WHEN utm_campaign = 'brand' THEN 'paid_brand'
