4048ad312113bd5e73ca04d0f409a63d08833bcd
Haerin <109455136+HaeRin-OortCloud@users.noreply.github.com>
2023-07-14 14:50:30 +0900

    Add files via upload

---
+++ Conversion Funnels.sql
@@ -0,0 +1,115 @@
+-- Building Conversion Funnels
+
+-- STEP 1: Select all pageviews for relevant sessions
+-- STEP 2: Identify each pageview as the specific funnel step
+-- STEP 3: Create the session-level conversion funnel view
+-- STEP 4: Aggregate the data to assess funnel performance
+
+USE mavenfuzzyfactory
+
+SELECT * FROM website_pageviews WHERE website_session_id = 1059
+
+SELECT
+	website_session_id,
+    MAX(products_page) AS product_made_it,
+    MAX(mrfuzzy_page) AS mrfuzzy_made_it,
+    MAX(cart_page) AS cart_made_it
+FROM(
+SELECT
+	website_sessions.website_session_id,
+    website_pageviews.pageview_url,
+    CASE WHEN pageview_url = '/products' THEN 1 ELSE 0 END AS products_page,
+    CASE WHEN pageview_url = '/the-original-mr-fuzzy' THEN 1 ELSE 0 END AS mrfuzzy_page,
+    CASE WHEN pageview_url = '/cart' THEN 1 ELSE 0 END AS cart_page
+FROM website_sessions
+	LEFT JOIN website_pageviews
+		ON website_sessions.website_session_id = website_pageviews.website_session_id
+WHERE website_sessions.created_at BETWEEN '2014-01-01' AND '2014-02-01'
+	AND website_pageviews.pageview_url IN ('/lander-2', '/products', '/the-original-mr-fuzzy', '/cart')
+ORDER BY
+	website_sessions.website_session_id,
+    website_pageviews.created_at
+) AS pageview_lebel
+
+GROUP BY
+	website_session_id
+;
+
+
+CREATE TEMPORARY TABLE session_level_made_it_flags_demo
+SELECT
+	website_session_id,
+    MAX(products_page) AS product_made_it,
+    MAX(mrfuzzy_page) AS mrfuzzy_made_it,
+    MAX(cart_page) AS cart_made_it
+FROM(
+SELECT
+	website_sessions.website_session_id,
+    website_pageviews.pageview_url,
+    CASE WHEN pageview_url = '/products' THEN 1 ELSE 0 END AS products_page,
+    CASE WHEN pageview_url = '/the-original-mr-fuzzy' THEN 1 ELSE 0 END AS mrfuzzy_page,
+    CASE WHEN pageview_url = '/cart' THEN 1 ELSE 0 END AS cart_page
+FROM website_sessions
+	LEFT JOIN website_pageviews
+		ON website_sessions.website_session_id = website_pageviews.website_session_id
+WHERE website_sessions.created_at BETWEEN '2014-01-01' AND '2014-02-01'
+	AND website_pageviews.pageview_url IN ('/lander-2', '/products', '/the-original-mr-fuzzy', '/cart')
+ORDER BY
+	website_sessions.website_session_id,
+    website_pageviews.created_at
+) AS pageview_lebel
+
+GROUP BY
+	website_session_id
+;
+
+
+SELECT
+	COUNT(DISTINCT website_session_id) AS sessions,
+    COUNT(DISTINCT CASE WHEN product_made_it = 1 THEN website_session_id ELSE NULL END) AS to_products,
+    COUNT(DISTINCT CASE WHEN mrfuzzy_made_it = 1 THEN website_session_id ELSE NULL END) AS to_mrfuzzy,
+    COUNT(DISTINCT CASE WHEN cart_made_it = 1 THEN website_session_id ELSE NULL END) AS to_cart
+FROM session_level_made_it_flags_demo;
+
+-- Translate those counts to click rates for final output part 2 (click rates)
+
+SELECT
+	COUNT(DISTINCT website_session_id) AS sessions,
+    COUNT(DISTINCT CASE WHEN product_made_it = 1 THEN website_session_id ELSE NULL END) 
+		/COUNT(DISTINCT website_session_id) AS lander_clickthrough_rate,
+    COUNT(DISTINCT CASE WHEN mrfuzzy_made_it = 1 THEN website_session_id ELSE NULL END) 
+		/COUNT(DISTINCT CASE WHEN product_made_it = 1 THEN website_session_id ELSE NULL END) AS products_clickthrough_rate,
+    COUNT(DISTINCT CASE WHEN cart_made_it = 1 THEN website_session_id ELSE NULL END) 
+		/COUNT(DISTINCT CASE WHEN mrfuzzy_made_it = 1 THEN website_session_id ELSE NULL END) AS mrfuzzy_clickthrough_rate
+FROM session_level_made_it_flags_demo;
+
+
+
+-- Analyzing Conversion Funnel Test (Compare the origianl /billing page with an updated /billing-2 page)
+SELECT
+	MIN(website_pageviews.website_pageview_id) AS first_pv_id
+FROM website_pageviews
+WHERE pageview_url = '/billing-2';
+
+-- first_pv_id = 5350
+
+SELECT
+	billing_version_seen,
+    COUNT(DISTINCT website_session_id) AS sessions,
+    COUNT(DISTINCT order_id) AS orders,
+    COUNT(DISTINCT order_id) / COUNT(DISTINCT website_session_id) AS billing_to_order_rt
+FROM(
+SELECT
+	website_pageviews.website_session_id,
+    website_pageviews.pageview_url AS billing_version_seen,
+    orders.order_id
+FROM website_pageviews
+	LEFT JOIN orders
+		ON website_pageviews.website_session_id = orders.website_session_id
+WHERE website_pageviews.website_pageview_id >= 53550 -- first pageview_id where test was live
+	AND website_pageviews.created_at < '2012-11-10' -- time of request
+    AND website_pageviews.pageview_url IN ('/billing', '/billing-2')
+) AS billing_session_w_orders
+GROUP BY
+	billing_version_seen;
+    
